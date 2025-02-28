7e19543735996e0dd7ab7ea9de2ac30e8454e3ce
Ahmed Jamal <ahmedjamal22290@gmail.com>
2025-02-28 13:40:47 +0200

    create float button

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:todo_list_app/components/home_view_body.dart';
+import 'package:todo_list_app/constants.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
   static String id = "HomeView";
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: HomeViewBody(),
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Color(0xff2A2A2A),
+        elevation: 350,
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
+          size: 29,
+        ),
+      ),
+      body: const HomeViewBody(),
     );
   }
 }
