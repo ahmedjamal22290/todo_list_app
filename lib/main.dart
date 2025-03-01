75a42e9d9142aea089bd4b7419e6ceecbab64cda
Ahmed Jamal <ahmedjamal22290@gmail.com>
2025-03-01 14:29:16 +0200

    create hive box

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'package:todo_list_app/constants.dart';
+import 'package:todo_list_app/models/todo_model.dart';
 import 'package:todo_list_app/views/add_todo_view.dart';
 import 'package:todo_list_app/views/home_view.dart';
 
-void main() {
+void main() async {
+  Hive.initFlutter();
+  Hive.registerAdapter(TodoModelAdapter());
+  Hive.openBox<TodoModel>(kBoxName);
   runApp(const TodoListApp());
 }
 
