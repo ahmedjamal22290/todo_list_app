10df5758ac35e34c45f3e0c6851407db570cee82
Ahmed Jamal <ahmedjamal22290@gmail.com>
2025-03-01 13:06:04 +0200

    create time field widget

--- lib/components/add_todo_body.dart
+++ lib/components/add_todo_body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:todo_list_app/Widgets/descripition_field_widget.dart';
 import 'package:todo_list_app/Widgets/titile_field_widget.dart';
+import 'package:todo_list_app/constants.dart';
 
 class AddTodoBody extends StatelessWidget {
   const AddTodoBody({
@@ -17,7 +18,43 @@ class AddTodoBody extends StatelessWidget {
           padding: EdgeInsets.symmetric(horizontal: 16.0),
           child: DsecriptionFieldWidget(),
         ),
+        SizedBox(height: 30),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0),
+          child: TimeFieldWidget(),
+        )
       ],
+    );
+  }
+}
+
+class TimeFieldWidget extends StatelessWidget {
+  const TimeFieldWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      readOnly: true,
+      decoration: InputDecoration(
+        labelText: 'Select Date',
+        suffixIcon: const Icon(Icons.calendar_today_rounded),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide(color: kHighlightColor),
+        ),
+      ),
+      onTap: () async {
+        DateTime? picked = await showDatePicker(
+          context: context,
+          firstDate: DateTime.now(),
+          lastDate: DateTime(2100),
+        );
+      },
     );
   }
 }
--- lib/views/add_todo_view.dart
+++ lib/views/add_todo_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:todo_list_app/Widgets/titile_field_widget.dart';
-import 'package:todo_list_app/constants.dart';
+import 'package:todo_list_app/components/add_todo_body.dart';
 
 class AddTodoView extends StatelessWidget {
   const AddTodoView({super.key});
@@ -9,31 +8,7 @@ class AddTodoView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(
-        children: [
-          const TitleFieldWidget(),
-          const SizedBox(height: 30),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: TextFormField(
-              maxLines: 5,
-              decoration: InputDecoration(
-                label: const Text(
-                  'Description ',
-                  style: TextStyle(color: Color(0xffE0E0E0)),
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                  borderSide: BorderSide(color: kHighlightColor),
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
+      body: const AddTodoBody(),
     );
   }
 }
