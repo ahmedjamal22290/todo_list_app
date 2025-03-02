783f0bbdf69b04072bba30a60a319f29e602f11a
Ahmed Jamal <ahmedjamal22290@gmail.com>
2025-03-02 13:44:53 +0200

    refactor cubit

--- lib/cubit/fetch_todo_cubit.dart
+++ lib/cubit/fetch_todo_cubit.dart
@@ -6,18 +6,40 @@ import 'package:todo_list_app/models/todo_model.dart';
 
 class FetchTodoCubit extends Cubit<FetchTodoState> {
   FetchTodoCubit() : super(FetchTodoInitial());
+
   fetchPendingTodos() {
     var todosBox = Hive.box<TodoModel>(kBoxName).values.toList();
-    List<TodoModel> pendingTodos = [];
-    for (int i = 0; i < todosBox.length; i++) {
-      if (todosBox[i].status == 'P') {
-        pendingTodos.add(todosBox[i]);
-      }
-    }
+    List<TodoModel> pendingTodos =
+        todosBox.where((todo) => todo.status == 'P').toList();
+
     if (pendingTodos.isEmpty) {
-      emit(EmptyListState());
+      emit(EmptyPendingListState());
     } else {
-      emit(SuccessfulyFetchState(list: pendingTodos));
+      emit(SuccessfulyFetchPendingState(list: pendingTodos));
+    }
+  }
+
+  fetchOverdueTodos() {
+    var todosBox = Hive.box<TodoModel>(kBoxName).values.toList();
+    List<TodoModel> overdueTodos =
+        todosBox.where((todo) => todo.status == 'O').toList();
+
+    if (overdueTodos.isEmpty) {
+      emit(EmptyOverdueListState());
+    } else {
+      emit(SuccessfulyFetchOverDueState(list: overdueTodos));
+    }
+  }
+
+  fetchCompleteTodos() {
+    var todosBox = Hive.box<TodoModel>(kBoxName).values.toList();
+    List<TodoModel> completeTodos =
+        todosBox.where((todo) => todo.status == 'C').toList();
+
+    if (completeTodos.isEmpty) {
+      emit(EmptyCompleteListState());
+    } else {
+      emit(SuccessfulyFetchCompleteState(list: completeTodos));
     }
   }
 }
--- lib/cubit/fetch_todo_states.dart
+++ lib/cubit/fetch_todo_states.dart
@@ -4,10 +4,26 @@ abstract class FetchTodoState {}
 
 class FetchTodoInitial extends FetchTodoState {}
 
-class EmptyListState extends FetchTodoState {}
+class EmptyPendingListState extends FetchTodoState {}
 
-class SuccessfulyFetchState extends FetchTodoState {
+class EmptyOverdueListState extends FetchTodoState {}
+
+class EmptyCompleteListState extends FetchTodoState {}
+
+class SuccessfulyFetchPendingState extends FetchTodoState {
   final List<TodoModel> list;
 
-  SuccessfulyFetchState({required this.list});
+  SuccessfulyFetchPendingState({required this.list});
+}
+
+class SuccessfulyFetchOverDueState extends FetchTodoState {
+  final List<TodoModel> list;
+
+  SuccessfulyFetchOverDueState({required this.list});
+}
+
+class SuccessfulyFetchCompleteState extends FetchTodoState {
+  final List<TodoModel> list;
+
+  SuccessfulyFetchCompleteState({required this.list});
 }
