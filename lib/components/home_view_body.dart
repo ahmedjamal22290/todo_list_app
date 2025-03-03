173592ebd13fc6892587f9de2335d47017dade23
Ahmed Jamal <ahmedjamal22290@gmail.com>
2025-03-03 14:37:53 +0200

    add options

--- lib/components/home_view_body.dart
+++ lib/components/home_view_body.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:hive_flutter/adapters.dart';
 import 'package:todo_list_app/Widgets/custom_animated_bar.dart';
 import 'package:todo_list_app/Widgets/selected_todo_item.dart';
 import 'package:todo_list_app/Widgets/todo_item.dart';
 import 'package:todo_list_app/constants.dart';
 import 'package:todo_list_app/cubit/fetch_todo_cubit.dart';
 import 'package:todo_list_app/cubit/fetch_todo_states.dart';
-import 'package:todo_list_app/models/todo_model.dart';
 
 class HomeViewBody extends StatefulWidget {
   const HomeViewBody({super.key});
@@ -18,7 +16,12 @@ class HomeViewBody extends StatefulWidget {
 
 class _HomeViewBodyState extends State<HomeViewBody> {
   final List<String> categories = ["Complete", "Pending", "Overdue"];
-  bool inSelectabledMood = false;
+  final List<String> buttonsInSelectedMode = [
+    'Edit',
+    'Confirm',
+    'Delete',
+  ];
+  bool inSelectabledMode = false;
 
   int selectedIndex = 1;
   @override
@@ -50,21 +53,45 @@ class _HomeViewBodyState extends State<HomeViewBody> {
     return Column(
       children: [
         Row(
-          children: List.generate(
-            categories.length,
-            (index) {
-              return GestureDetector(
-                onTap: () {
-                  updateCategory(index);
-                },
-                child: CutsomAnimatedBar(
-                  index: index,
-                  selectedIndex: selectedIndex,
-                  categories: categories,
+          children: inSelectabledMode
+              ? List.generate(buttonsInSelectedMode.length, (index) {
+                  return GestureDetector(
+                    child: Container(
+                      height: 60,
+                      width: MediaQuery.of(context).size.width / 3,
+                      padding: const EdgeInsets.only(top: 5),
+                      margin: const EdgeInsets.only(top: 30),
+                      decoration: BoxDecoration(
+                          border: const Border.symmetric(
+                              vertical:
+                                  BorderSide(color: Colors.white, width: 0.9)),
+                          color: kHighlightColor),
+                      child: Center(
+                          child: Text(
+                        buttonsInSelectedMode[index],
+                        style: const TextStyle(
+                          fontSize: 23,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )),
+                    ),
+                  );
+                })
+              : List.generate(
+                  categories.length,
+                  (index) {
+                    return GestureDetector(
+                      onTap: () {
+                        updateCategory(index);
+                      },
+                      child: CutsomAnimatedBar(
+                        index: index,
+                        selectedIndex: selectedIndex,
+                        categories: categories,
+                      ),
+                    );
+                  },
                 ),
-              );
-            },
-          ),
         ),
         Expanded(
           child: BlocBuilder<FetchTodoCubit, FetchTodoState>(
@@ -76,13 +103,13 @@ class _HomeViewBodyState extends State<HomeViewBody> {
                 return ListView.builder(
                   itemCount: list.length,
                   itemBuilder: (context, index) {
-                    if (!inSelectabledMood) {
+                    if (!inSelectabledMode) {
                       return TodoItem(
                           onLongPress: () {
-                            if (inSelectabledMood) {
-                              inSelectabledMood = false;
+                            if (inSelectabledMode) {
+                              inSelectabledMode = false;
                             } else {
-                              inSelectabledMood = true;
+                              inSelectabledMode = true;
                             }
                             setState(() {});
                           },
@@ -91,10 +118,10 @@ class _HomeViewBodyState extends State<HomeViewBody> {
                       bool selected = false;
                       return SelectedTodoItem(
                         onLongPress: () {
-                          if (inSelectabledMood) {
-                            inSelectabledMood = false;
+                          if (inSelectabledMode) {
+                            inSelectabledMode = false;
                           } else {
-                            inSelectabledMood = true;
+                            inSelectabledMode = true;
                           }
                           setState(() {});
                         },
