b84ada9af90233442e0bde89814ddbeac222b64c
Ahmed Jamal <ahmedjamal22290@gmail.com>
2025-03-05 15:27:30 +0200

    add style in add todo view

--- lib/components/add_todo_body.dart
+++ lib/components/add_todo_body.dart
@@ -32,82 +32,101 @@ class _AddTodoBodyState extends State<AddTodoBody> {
   Widget build(BuildContext context) {
     return Form(
       key: formKey,
-      child: Column(
+      child: Stack(
         children: [
-          TitleFieldWidget(
-            controller: _controller,
-            onChanged: (value) {
-              _controller.text = value;
-              todo.title = value;
-              setState(() {});
-            },
-          ),
-          const SizedBox(height: 30),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: DsecriptionFieldWidget(
-              onSaved: (value) {
-                todo.description = value;
-                setState(() {});
-              },
+          Container(
+            height: MediaQuery.sizeOf(context).height * 0.49,
+            decoration: BoxDecoration(
+              borderRadius: const BorderRadius.only(
+                  bottomRight: Radius.circular(80),
+                  bottomLeft: Radius.circular(80)),
+              color: kSecondColor,
             ),
           ),
-          const SizedBox(height: 30),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: TimeFieldWidget(
-              controller: _dateController,
-              onTap: () async {
-                DateTime? picked = await showDatePicker(
-                  context: context,
-                  firstDate: DateTime.now(),
-                  lastDate: DateTime(2035),
-                );
-                if (picked != null) {
-                  setState(() {
-                    _dateController.text =
-                        "${picked.year}-${picked.month.toString().padLeft(2, '0')}-${picked.day.toString().padLeft(2, '0')}";
-                    todo.date = picked;
-                  });
-                }
-              },
-            ),
-          ),
-          const SizedBox(height: 40),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                CustomButton(
-                  onTap: () {
-                    Navigator.pop(context);
+          Column(
+            children: [
+              TitleFieldWidget(
+                controller: _controller,
+                onChanged: (value) {
+                  _controller.text = value;
+                  todo.title = value;
+                  setState(() {});
+                },
+              ),
+              const SizedBox(height: 30),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: DsecriptionFieldWidget(
+                  onSaved: (value) {
+                    todo.description = value;
+                    setState(() {});
                   },
-                  icon: const Icon(
-                    Icons.cancel_outlined,
-                    color: Colors.red,
-                  ),
-                  text: 'Cancel',
                 ),
-                CustomButton(
-                  onTap: () {
-                    if (formKey.currentState!.validate()) {
-                      formKey.currentState!.save();
-                      Hive.box<TodoModel>(kBoxName).add(todo);
-                      BlocProvider.of<FetchTodoCubit>(context)
-                          .fetchPendingTodos();
-                      Navigator.pop(context);
+              ),
+              const SizedBox(height: 30),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: TimeFieldWidget(
+                  controller: _dateController,
+                  onTap: () async {
+                    DateTime? picked = await showDatePicker(
+                      context: context,
+                      firstDate: DateTime.now(),
+                      lastDate: DateTime(2035),
+                    );
+                    if (picked != null) {
+                      setState(() {
+                        _dateController.text =
+                            "${picked.year}-${picked.month.toString().padLeft(2, '0')}-${picked.day.toString().padLeft(2, '0')}";
+                        todo.date = picked;
+                      });
                     }
                   },
-                  icon: const Icon(
-                    Icons.check,
-                    color: Colors.green,
-                  ),
-                  text: 'Save',
                 ),
-              ],
-            ),
-          )
+              ),
+              const SizedBox(height: 40),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    CustomButton(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      icon: const Icon(
+                        Icons.cancel_outlined,
+                        color: Colors.red,
+                      ),
+                      text: 'Cancel',
+                    ),
+                    CustomButton(
+                      onTap: () {
+                        if (formKey.currentState!.validate()) {
+                          formKey.currentState!.save();
+                          Hive.box<TodoModel>(kBoxName).add(todo);
+                          BlocProvider.of<FetchTodoCubit>(context)
+                              .fetchPendingTodos();
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                              backgroundColor: kSecondColor,
+                              content: const Text(
+                                'Added Succefly',
+                                style: TextStyle(color: Colors.white),
+                              )));
+                          Navigator.pop(context);
+                        }
+                      },
+                      icon: const Icon(
+                        Icons.check,
+                        color: Colors.green,
+                      ),
+                      text: 'Save',
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
