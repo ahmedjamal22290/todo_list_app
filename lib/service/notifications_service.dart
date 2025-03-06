dce0d19d282f9a4df8bca69d89c5a3013f7a83b5
Ahmed Jamal <ahmedjamal22290@gmail.com>
2025-03-06 16:57:01 +0200

    finish notification setvice

--- lib/service/notifications_service.dart
+++ lib/service/notifications_service.dart
@@ -1,1 +1,40 @@
-class
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class NotiService {
+  final notificationsPlugin = FlutterLocalNotificationsPlugin();
+  bool _isInialized = false;
+  bool get isInialized => _isInialized;
+  Future<void> initNotification() async {
+    if (_isInialized) return;
+    const initAndroidSetting =
+        AndroidInitializationSettings('@mipmap-hdpi/ic_launcher.png');
+    const initIosSettings = DarwinInitializationSettings(
+      requestAlertPermission: true,
+      requestBadgePermission: true,
+      requestSoundPermission: true,
+    );
+    const initSettings = InitializationSettings(
+      android: initAndroidSetting,
+      iOS: initIosSettings,
+    );
+    await notificationsPlugin.initialize(initSettings);
+  }
+
+  NotificationDetails notificationDetails() {
+    return const NotificationDetails(
+        android: AndroidNotificationDetails(
+          'Daily channel Id',
+          'Daily noti',
+          channelDescription: 'daily noti descrip',
+          importance: Importance.max,
+          priority: Priority.high,
+        ),
+        iOS: DarwinNotificationDetails());
+  }
+
+  Future<void> showNotificatino(
+      {int id = 0, String? title, String? body}) async {
+    return notificationsPlugin.show(
+        id, title, body, const NotificationDetails());
+  }
+}
